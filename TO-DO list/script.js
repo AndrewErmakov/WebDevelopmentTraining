9ded4c055c923c1312e2feaab51bdc74989ee8d6
Andrew Ermakov <andrew.67@list.ru>
2020-03-12 00:41:03 +0500

    implemented saving the current state of the check sheet, changed the background, implemented cleaning the entire list

--- TO-DO list/script.js
+++ TO-DO list/script.js
@@ -1,12 +1,40 @@
-let addTaskButton= document.querySelector('.addTaskButton');
-var listTasks = document.getElementById('listTasks');
-
-
-addTaskButton.onclick = function() {
-	let newTask = document.querySelector('.taskDescription').value;
-	alert('Вы добавили новую задачу: ' + newTask);
-	var entry = document.createElement('li', checked = "False");
-	entry.appendChild(document.createTextNode(newTask));
-	listTasks.appendChild(entry);
+window.onload = function () {
+	let listTasks = [];
+	if (localStorage.getItem('superKey') !== undefined) {
+		listTasks = JSON.parse(localStorage.getItem('superKey'));
+		printTasks();
 	}
+	document.getElementById('addButton').onclick = function () {
+		let newTask = document.getElementById('taskDescription').value;
+		let temp = {};
+		temp.todo = newTask;
+		temp.check = false;
+		listTasks[listTasks.length] = temp;
+		printTasks();
+		localStorage.setItem('superKey', JSON.stringify(listTasks));
+		document.getElementById('taskDescription').value = '';
+	};
+	document.getElementById('clearAll').onclick = function () {
+		listTasks = [];
+		printTasks();
+		localStorage.setItem('superKey', JSON.stringify(listTasks));
+	}
+
+
+	function printTasks() {
+		let out = '';
+		for (let task in listTasks){
+			if (listTasks[task].check === true){
+				out += '<input id="flag" type="checkbox" checked>';
+			}
+			else {
+				out += '<input id="flag" type="checkbox">';
+			}
+			out += listTasks[task].todo + '<br>';
+
+		}
+		document.getElementById('output').innerHTML = out;
+	}
+
+};
 
