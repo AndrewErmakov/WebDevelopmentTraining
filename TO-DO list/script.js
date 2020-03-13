2ccf5fda2693bb0b3b8b50c2adcaf26bfab20d30
Andrew Ermakov <andrew.67@list.ru>
2020-03-14 00:49:06 +0500

    Implemented deletion of completed (selected) tasks, added a link to the repository

--- TO-DO list/script.js
+++ TO-DO list/script.js
@@ -26,21 +26,56 @@ window.onload = function () {
 		localStorage.setItem('superKey', JSON.stringify(listTasks));
 	}
 
+	function checkTask() {
+		let checkboxes = document.getElementsByClassName("Checkboxes");
+		for (let i = 0; i < listTasks.length; i++){
+			if (checkboxes[i].checked){
+				listTasks[i].check = true;
+			}
+			else {
+				listTasks[i].check = false;
+			}
+			localStorage.setItem('superKey', JSON.stringify(listTasks));
+		}
+	}
+	document.getElementById('output').onclick = function (e) {
+		if (e.target.classList.contains("Checkboxes")){
+			checkTask();
+			printTasks();
+		}
+	}
 
 	function printTasks() {
 		let out = '';
-		for (let i = 0;i < listTasks.length; i++){
+		for (let i = 0; i < listTasks.length; i++){
 			if (listTasks[i].check == true){
-				out += '<input id="flag" type="checkbox" checked>';
+				out += '<input id="' + String(i)+ '" class="Checkboxes" type="checkbox" checked>';
+				out += '<s>' + listTasks[i].todo + '</s><br>';
+
 			}
 			else {
-				out += '<input id="flag" type="checkbox">';
+				out += '<input id="' + String(i)+ '" class="Checkboxes" type="checkbox">' + listTasks[i].todo + '<br>';
 			}
-			out += listTasks[i].todo + '<br>';
-
 		}
 		document.getElementById('output').innerHTML = out;
 	}
 
-};
+
+	document.getElementById('delButton').onclick = function () {
+		let i = 0;
+		while (i < listTasks.length){
+			if (listTasks[i].check == true){
+				listTasks.splice(i, 1);
+				// console.log(listTasks);
+			}
+			else {
+				i++;
+			}
+		}
+		printTasks();
+		localStorage.setItem('superKey', JSON.stringify(listTasks));
+		document.getElementById('taskDescription').value = '';
+	}
+}
+
 
