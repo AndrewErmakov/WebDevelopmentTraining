9cbb72e44fb91828adcb940a304ce0b893fe77c1
Andrew Ermakov <andrew.67@list.ru>
2020-06-07 02:41:59 +0500

    created a layout service for determining the weather in the city

---
+++ WeatherApp/weather/views.py
@@ -0,0 +1,34 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    # appid = '439d4b804bc8187953eb36d2a8c26a02'
+    appid = '4249f3f743e648a27db9b2d9cde423f0'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+
+        response = requests.get(url.format(city.name)).json()
+
+        city_info = {
+            'city': city.name,
+            'temp': response['main']['temp'],
+            'icon': response['weather'][0]['icon']
+        }
+
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form':form}
+    return render(request, 'weather/index.html', context)
