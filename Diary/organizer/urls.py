e4ac1342906cd0d6aed3b9ec3d6ea92f68c53341
Andrew Ermakov <andrew.67@list.ru>
2020-07-26 00:11:12 +0500

    Added updating and deleting notes in the diary

--- Diary/organizer/urls.py
+++ Diary/organizer/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.NoteListView.as_view(), name='home'),
     path('note/<int:pk>/', views.NoteDetailView.as_view(), name='details_note'),
     path('note/new/', views.NoteAddView.as_view(), name='add_new_note'),
+    path('note/<int:pk>/edit/', views.NoteUpdateView.as_view(), name='note_edit'),
+    path('note/<int:pk>/delete/', views.NoteDeleteView.as_view(), name='note_delete'),
 ]
--- Diary/organizer/views.py
+++ Diary/organizer/views.py
@@ -1,22 +1,55 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import DiaryNote
 
 
 class NoteListView(ListView):
+    """
+    Класс просмотра всех записей в ежедневнике
+    """
     model = DiaryNote
     template_name = 'home.html'
     queryset = DiaryNote.objects.all().order_by('date')[:10]
 
 
 class NoteDetailView(DetailView):
+    """
+    Класс просмотра подробной инфы о выбранной записи в ежедневнике
+    """
+
     def get(self, request, pk):
         note = DiaryNote.objects.get(pk=pk)
         return render(request, 'details_note.html', {'note': note})
 
 
 class NoteAddView(CreateView):
+    """
+    Класс добавления новой записи в ежедневник
+    """
     model = DiaryNote
     template_name = 'add_new_note.html'
     fields = '__all__'
+
+
+class NoteUpdateView(UpdateView):
+    """
+    Класс обновления выбранной записи в ежедневнике
+    """
+    model = DiaryNote
+    template_name = 'note_edit.html'
+    fields = [
+        'date',
+        'note_heading',
+        'text'
+    ]
+
+
+class NoteDeleteView(DeleteView):
+    """
+    Класс удаления выбранной записи в ежедневнике
+    """
+    model = DiaryNote
+    template_name = 'note_delete.html'
+    success_url = reverse_lazy('home')  # перенаправление на страницу home после того, как удаление выполнено
