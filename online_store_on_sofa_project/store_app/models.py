48f315ae152530ae02b7918c711e418381a576b7
andrew.67@list.ru <andrew.67@list.ru>
2021-01-19 23:48:29 +0500

    make a blank of feedback's form

--- online_store_on_sofa_project/store_app/models.py
+++ online_store_on_sofa_project/store_app/models.py
@@ -127,7 +127,8 @@ class Recipient(models.Model):
     """Модель получателя заказа"""
     name_recipient = models.CharField(max_length=50, verbose_name='Имя получателя заказа', blank=True, null=True)
     surname_recipient = models.CharField(max_length=50, verbose_name='Фамилия получателя заказа', blank=True, null=True)
-    phone_recipient = models.CharField(max_length=14, verbose_name='Номер телефона получателя заказа', blank=True, null=True)
+    phone_recipient = models.CharField(max_length=14, verbose_name='Номер телефона получателя заказа', blank=True,
+                                       null=True)
 
     class Meta:
         verbose_name_plural = 'Получатели заказа'
@@ -176,3 +177,21 @@ class ProductsInOrder(models.Model):
         verbose_name_plural = 'Товары в заказах'
         verbose_name = 'Товар в заказе'
         ordering = ['product', 'count_product_in_order']
+
+
+class FeedBackWithClient(models.Model):
+    """Модель обращения клиентов с просьбой об обратной связи"""
+    name_client = models.CharField(max_length=50, verbose_name='Имя клиента',
+                                   validators=[validators.MinLengthValidator(5)])
+    phone_client = models.CharField(max_length=14, verbose_name='Номер телефона для обратной связи')
+    email_client = models.EmailField(verbose_name='Электронная почта для обратной связи',
+                                     validators=[validators.MinLengthValidator(5)])
+    given_feedback = models.BooleanField(verbose_name='Дана ли обратная связь?', default=False)
+
+    def __str__(self):
+        return f'Заявка на обратную связь №{str(self.pk).zfill(6)}'
+
+    class Meta:
+        verbose_name_plural = 'Заявки на обратную связь'
+        verbose_name = 'Заявка на обратную связь'
+        ordering = ['name_client', 'phone_client', 'email_client', 'given_feedback']
